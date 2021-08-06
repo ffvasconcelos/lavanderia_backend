ac340c5ea8d25c488a71dfda342ad2b908be3168
Francisco Vasconcelos <fr4nk2000@gmail.com>
2021-08-05 21:04:27 -0300

    Adicionando Cors ao servidor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const Cors = require('cors')
 
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(Cors())
 
 //routes
 app.use(require('./routes/index'));
